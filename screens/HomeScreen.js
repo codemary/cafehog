f266ed5673cfc6f238ff7a34376f629b803a8bd7
codemary <mariamfatima.co@gmail.com>
2017-09-23 00:46:06 +0530

    add list view to the home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,7 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  FlatList,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -12,16 +13,53 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
+
+const rows = [
+  { id: 0, text: 'View' },
+  { id: 1, text: 'Text' },
+  { id: 2, text: 'Image' },
+  { id: 3, text: 'ScrollView' },
+  { id: 4, text: 'ListView' },
+]
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+})
+
+const extractKey = ({ id }) => id
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Activity',
   };
 
+  renderItem = ({ item }) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
+
+
   render() {
     return (
-      <View >
 
-      </View>
+      <FlatList
+        style={styles.container}
+        data={rows}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+      />
     );
   }
 }
